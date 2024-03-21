25adea888117f88c5a9a06ebd68e0981615b49d7
Onkar Ijare <ijareonkar2184@gmail.com>
2024-03-21 16:02:46 +0530

    img slideshow

--- login.js
+++ login.js
@@ -6,4 +6,12 @@ function switchText() {
       x.innerHTML = "I am a student.";
     }
     text.style.cursor="pointer";
-  }
+}
+
+const icon=document.querySelectorAll(".inputBox");
+
+icon.forEach((inputBox) =>{
+  inputBox.addEventListener("click",=>{
+    let show=inputBox.parentElement.parentElement.querySelectorAll()
+  })
+})
